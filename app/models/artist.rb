85dfe2c867342666a9c3091d1401457388ae0f6f
Hoon Lee <yl.hoony@gmail.com>
2018-01-01 10:59:55 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -16,7 +16,7 @@ class Artist < ActiveRecord::Base
 
   def genre_count
     #return the number of genres associated with the artist
-    self.songs
+    self.songs.collect {|song| song.genre}.uniq.size
     binding.pry
   end
 end
