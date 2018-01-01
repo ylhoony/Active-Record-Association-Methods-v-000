b72aca7397471c05c8035e6bf1df327a65a6c3d2
Hoon Lee <yl.hoony@gmail.com>
2018-01-01 10:56:44 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -6,8 +6,8 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
-    self.songs.first
-    binding.pry
+    self.songs.first.genre
+    # binding.pry
   end
 
   def song_count
